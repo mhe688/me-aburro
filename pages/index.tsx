0810b132bf1af336239c027d2c8f67663c158821
mhe688 <118557823+mhe688@users.noreply.github.com>
2022-11-18 15:53:40 -0300

    Imported from Quest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import Link from 'next/link'
+import type { NextPage } from 'next';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import questTheme from 'QuestDesignSystem5LightTheme';
+import Dashboard1Comp from 'components/Dashboard1/Dashboard1';
 
-export default function IndexPage() {
+const Home: NextPage = () => {
   return (
-    <div>
-      Hello World. <Link href="/about">About</Link>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={questTheme}>
+        <div>
+          <main>
+            <Dashboard1Comp />
+          </main>
+        </div>
+      </ThemeProvider>
+    </StyledEngineProvider>
   )
 }
+
+export default Home;
